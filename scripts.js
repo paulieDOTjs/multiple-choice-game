690ff11ae86ba7b7b6e58a6eb1480be8930967be
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2019-11-15 14:58:24 -0600

    added pages, got start button and header into html

---
+++ scripts.js
@@ -0,0 +1,64 @@
+function initGame(){
+    alert("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+} initGame;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* multiple choice game pseudo code
+
+
+make a start button (jumbotron maybe?? make an on click)
+make a timer (not sure yet) 15 seconds * number of questions
+make questions ()
+make answer to questions
+designate which answer is correct
+give prompt that user got it right/wrong
+give reward for right
+make reward change based on how much time is left
+give punishment for wrong
+make thing that will show next question
+make a score keeper
+make a high score keeper 
+make a time over screen 
+make a thing where they can enter their high score
+bootstrap that guy
+
+
+
+
+would be nice:
+
+sounds effects
+customizeable theme
+multiple quizzes
+add to portfolio
