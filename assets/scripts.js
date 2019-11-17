d15ee89886168d8dcfb31d5c7198ff4380b606cd
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2019-11-17 00:50:22 -0600

    game now progresses through questions when right/wrong answer is selected

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,70 @@
 
-function initGame (){
+function initGame() {
 
 
     const startButtonEl = document.getElementById("startGameButton");
     const startContainerEl = document.getElementById("startContainer");
     const cardContainerEl = document.getElementById("questionContainer");
-    const questions = [
-        ["Question 1:", "When in the key of C major what chord will be played to execute a tritone substitution and cadence on a I chord?", "C#7", "F#add9", "G Maj7", "B dim"],
-        ["Question 2:", "What is your favorite color?"]
+    const questionNumberEl = document.getElementById("questionNumber");
+    const questionPromptEl = document.getElementById("questionPrompt");
+    questions = [
+        ["Question 5:", "In a 4/4 time signature what note gets one beat?", "eighth note", "quarter note", "whole note", "half note", 1],
+        ["Question 5:", "When in the key of C major what chord will be played to execute a tritone substitution and cadence on a I chord?", "C#7", "F#add9", "G Maj7", "B dim", 0],
 
-]
+    ]
 
-    console.log(questions[1][0])
+
 
     function startGame() {
         startContainerEl.setAttribute("style", "display: none");
         cardContainerEl.setAttribute("style", "display: inline");
+        let currentQuestion = 0;
+
+        function checkGameProgress(){
+            if (currentQuestion<questions.length){
+                prepareQuestion();
+            } else{
+                console.log("You win!")
+            }
+
+        }
+
+        function prepareQuestion() {
+
+            questionNumberEl.innerHTML = questions[currentQuestion][0];
+            questionPromptEl.innerHTML = questions[currentQuestion][1];
+    
+            const answersEl = document.querySelectorAll(".answers-js");
+            console.log(answersEl);
+    
+            for (let i = 0; i < answersEl.length; i++) {
+                const answerEl = answersEl[i];
+                answerEl.innerHTML = questions[currentQuestion][i + 2]
+    
+                answerEl.addEventListener("click", function () {
+                    const chosenAnswer = i;
+                    if (chosenAnswer === questions[currentQuestion][6]) {
+                        console.log("winner");
+                    } else {
+                        console.log("loser");
+                    }
+                    currentQuestion++;
+                    checkGameProgress();
+                })
+            }
+        } checkGameProgress();
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
@@ -79,13 +128,13 @@ make reward change based on how much time is left
 give punishment for wrong
 make thing that will show next question
 make a score keeper
-make a high score keeper 
+make a high score keeper
 make a thing where they can enter their high score
 
 
 Put at the end
 make a timer (not sure yet) 15 seconds * number of questions
-make a time over screen 
+make a time over screen
 
 
 
