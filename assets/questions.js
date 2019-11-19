c2834d0fa31750a858ac1f8f5535c58f06627ae0
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2019-11-19 12:26:45 -0600

    moved questions to seperate js page

---
+++ assets/questions.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        number: "Question 1:",
+        title: "In a 4/4 time signature what note gets one beat?",
+        choices: ["eighth note", "quarter note", "half note", "whole note"],
+        answer: "quarter note"
+    },
+    {
+        number: "Question 2:",
+        title: "How many notes are in a major scale?",
+        choices: ["5", "12", "7", "9"],
+        answer: "7"
+    },
+    {
+        number: "Question 3:",
+        title: "What is the relative minor of C major?",
+        choices: ["Eb minor", "E minor", "Ab minor", "A minor"],
+        answer: "A minor"
+    },
+    {
+        number: "Question 4:",
+        title: "What is the chord progression used in a plagal cadence?",
+        choices: ["V-I", "IV-I", "vi-I", "V-vi"],
+        answer: "IV-I"
+    },
+    {
+        number: "Question 5:",
+        title: "When in the key of C major what chord will be played to execute a tritone substitution and cadence on a I chord?",
+        choices: ["C#7", "F#add9", "G Maj7", "B dim"],
+        answer: "C#7"
+    }]
--- assets/scripts.js
+++ assets/scripts.js
@@ -29,38 +29,6 @@ function initGame() {
         startGame();
     })
 
-    // The array of the questions and their answers 
-    const questions = [
-        {
-            number: "Question 1:",
-            title: "In a 4/4 time signature what note gets one beat?",
-            choices: ["eighth note", "quarter note", "half note", "whole note"],
-            answer: "quarter note"
-        },
-        {
-            number: "Question 2:",
-            title: "How many notes are in a major scale?",
-            choices: ["5", "12", "7", "9"],
-            answer: "7"
-        },
-        {
-            number: "Question 3:",
-            title: "What is the relative minor of C major?",
-            choices: ["Eb minor", "E minor", "Ab minor", "A minor"],
-            answer: "A minor"
-        },
-        {
-            number: "Question 4:",
-            title: "What is the chord progression used in a plagal cadence?",
-            choices: ["V-I", "IV-I", "vi-I", "V-vi"],
-            answer: "IV-I"
-        },
-        {
-            number: "Question 5:",
-            title: "When in the key of C major what chord will be played to execute a tritone substitution and cadence on a I chord?",
-            choices: ["C#7", "F#add9", "G Maj7", "B dim"],
-            answer: "C#7"
-        }]
 
     const defaultTime = questions.length * 15;
     let time = 0;
@@ -173,26 +141,23 @@ function initGame() {
 // make answer to questions
 // designate which answer is correct
 // check for right/wrong
+// give reward for right
 // give prompt that user got it right/wrong
+// make reward change based on how much time is left
+// make a score keeper
+// give punishment for wrong 
+// make a timer (not sure yet) 15 seconds * number of questions
+// make a time over screen
 
 
 
 
 
 // To Do:
-// give reward for right
-// make reward change based on how much time is left
-// give punishment for wrong
-// make a score keeper
 // make a high score keeper
-// make a thing where they can enter their high score
 // give link at the beginning to see high scores.
 
 
-
-// Put at the end
-// make a timer (not sure yet) 15 seconds * number of questions
-// make a time over screen
 
 
 
